327027b02ccd05f6ac204de544ecd49ddfe14ccc
yogesh0802 <119893355+yogesh0802@users.noreply.github.com>
2024-04-11 23:37:30 +0530

    Update Revenue.js

--- src/components/Revenue.js
+++ src/components/Revenue.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import { Link } from 'react-router-dom'
 
-import dashboard from "../img/revenue-dashboard.png";
+// import dashboard from "../img/revenue-dashboard.png";
 
 // const Revenue = () => {
 //   return (
